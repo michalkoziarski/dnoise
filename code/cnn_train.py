1de33fe73417ccc663ddd505bfb58c4672313fec
Michał Koziarski <michalkoziarski@gmail.com>
2015-12-20 12:04:55 +0100

    fixed dimensions

--- code/cnn_train.py
+++ code/cnn_train.py
@@ -15,7 +15,7 @@ if not os.path.exists('../models'):
     os.makedirs('../models')
 
 ds = utils.load_face_image(batch_size=BATCH_SIZE, split=SPLIT)
-cnn = CNN(input_shape=[None, 816, 816, 3], output_shape=[None, 16])
+cnn = CNN(input_shape=[None, 256, 256, 3], output_shape=[None, 16])
 saver = tf.train.Saver()
 epochs_completed = 0
 
