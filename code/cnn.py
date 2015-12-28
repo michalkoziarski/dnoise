bc0effe79f6b03bc9b9ff48ed50cf01b5fa932eb
Michał Koziarski <michalkoziarski@gmail.com>
2015-12-28 17:38:53 +0100

    training moved to CNN class

--- code/cnn.py
+++ code/cnn.py
@@ -1,19 +1,20 @@
 import tensorflow as tf
+import numpy as np
 
 
 class CNN:
-    def __init__(self, input_shape, output_shape, learning_rate=0.1, momentum=0.9):
+    def __init__(self, input_shape, output_shape, learning_rate=0.01, momentum=0.9):
         self.input_shape = input_shape
         self.output_shape = output_shape
         self.learning_rate = learning_rate
         self.momentum = momentum
+
         self.x = tf.placeholder(tf.float32, shape=[None] + input_shape)
         self.y_ = tf.placeholder(tf.float32, shape=[None] + output_shape)
         self.keep_prob = tf.placeholder(tf.float32)
-        self.y = self.inference()
-        self.train_op = self.train()
 
-    def inference(self):
+        # inference
+
         W = tf.Variable(tf.truncated_normal([5, 5, self.input_shape[2], 32], stddev=0.1))
         b = tf.Variable(tf.constant(0.1, shape=[32]))
         conv = tf.nn.conv2d(self.x, W, strides=[1, 1, 1, 1], padding='SAME')
@@ -39,19 +40,46 @@ class CNN:
 
         W = tf.Variable(tf.truncated_normal([1024] + self.output_shape, stddev=0.1))
         b = tf.Variable(tf.constant(0.1, shape=self.output_shape))
-        y = tf.nn.softmax(tf.matmul(dropout, W) + b)
+        self.y = tf.nn.softmax(tf.matmul(dropout, W) + b)
 
-        return y
+        # loss
 
-    def loss(self):
         cross_entropy = tf.nn.softmax_cross_entropy_with_logits(self.y, self.y_)
         cross_entropy_mean = tf.reduce_mean(cross_entropy)
 
-        return cross_entropy_mean
+        # training
 
-    def train(self):
-        return tf.train.MomentumOptimizer(self.learning_rate, self.momentum).minimize(self.loss())
+        self.train_op = tf.train.MomentumOptimizer(self.learning_rate, self.momentum).minimize(cross_entropy_mean)
 
-    def accuracy(self):
+        # accuracy
+
         correct_prediction = tf.equal(tf.argmax(self.y, 1), tf.argmax(self.y_, 1))
-        return tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+        self.accuracy_ = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+    def accuracy(self, dataset):
+        return np.mean([self.accuracy_.eval(feed_dict={
+               self.x: np.reshape(dataset.images[i].get(), [-1] + self.input_shape),
+               self.y_: [dataset.labels[i]],
+               self.keep_prob: 1.0
+        }) for i in range(dataset.length)]) * 100
+
+    def train(self, datasets, epochs=10, display_step=50):
+        with tf.Session() as sess:
+            sess.run(tf.initialize_all_variables())
+            batches_completed = 0
+
+            while datasets.train.epochs_completed < epochs:
+                if batches_completed % display_step == 0:
+                    print 'batch #%d, validation accuracy = %f%%' % \
+                          (batches_completed, self.accuracy(datasets.valid))
+
+                batch = datasets.train.batch()
+
+                self.train_op.run(feed_dict={
+                    self.x: np.reshape(batch.tensor(), [-1] + self.input_shape),
+                    self.y_: batch.labels, self.keep_prob: 0.5
+                })
+
+                batches_completed += 1
+
+            print 'test accuracy = %f%%' % self.accuracy(datasets.test)
--- code/cnn_train.py
+++ code/cnn_train.py
@@ -1,43 +1,7 @@
-import os
 import utils
-import tensorflow as tf
-import numpy as np
 
 from cnn import CNN
 
-
-EPOCHS = 50
-BATCH_SIZE = 16
-SPLIT = (0.8, 0.05, 0.15)
-MODEL_PATH = '../models/FaceImage.ckpt'
-
-if not os.path.exists('../models'):
-    os.makedirs('../models')
-
-ds = utils.load_face_image(batch_size=BATCH_SIZE, split=SPLIT, shape=(28, 28))
-cnn = CNN(input_shape=[28, 28, 3], output_shape=[16])
-saver = tf.train.Saver()
-epochs_completed = 0
-
-
-def accuracy(x):
-    return np.mean([cnn.accuracy().eval(feed_dict={
-           cnn.x: [x.images[i].get()],
-           cnn.y_: [x.labels[i]],
-           cnn.keep_prob: 1.0
-    }) for i in range(x.length)]) * 100
-
-
-with tf.Session() as sess:
-    sess.run(tf.initialize_all_variables())
-
-    while ds.train.epochs_completed < EPOCHS:
-        batch = ds.train.batch()
-        cnn.train_op.run(feed_dict={cnn.x: batch.tensor(), cnn.y_: batch.labels, cnn.keep_prob: 0.5})
-
-        if ds.train.epochs_completed > epochs_completed:
-            epochs_completed += 1
-            saver.save(sess, MODEL_PATH)
-            print 'epoch #%d, validation accuracy = %f%%' % (epochs_completed, accuracy(ds.valid))
-
-    print 'test accuracy = %f%%' % accuracy(ds.test)
+ds = utils.load_mnist()
+cnn = CNN(input_shape=[28, 28, 1], output_shape=[10])
+cnn.train(ds)
