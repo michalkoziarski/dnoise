974fc78720ac198311a1f4c1c0ee02284f6510ce
Michał Koziarski <michalkoziarski@gmail.com>
2016-01-22 22:57:43 +0100

    initialized noise module

---
+++ code/noise.py
@@ -0,0 +1,31 @@
+class Noise:
+    def __init__(self):
+        pass
+
+
+class GaussianNoise(Noise):
+    pass
+
+
+class SaltAndPepperNoise(Noise):
+    pass
+
+
+class QuantizationNoise(Noise):
+    pass
+
+
+class PhotonCountingNoise(Noise):
+    pass
+
+
+def mse(x, y):
+    pass
+
+
+def psnr(x, y):
+    pass
+
+
+def ssim(x, y):
+    pass
