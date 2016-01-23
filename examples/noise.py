5d4cefbf54d55189eac8142195e2d459b496f966
Michał Koziarski <michalkoziarski@gmail.com>
2016-01-23 17:30:17 +0100

    extended noise example

--- examples/noise.py
+++ examples/noise.py
@@ -1,4 +1,5 @@
 import code.utils
+import code.noise
 import os
 import urllib
 
@@ -14,5 +15,12 @@ if not os.path.exists(root_path):
 if not os.path.exists(img_path):
     urllib.urlretrieve(img_url, img_path)
 
-image = code.utils.Image(path=img_path).noisy(noise=code.utils.QuantizationNoise(q=0.5))
-image.display(size=(512, 512))
+image = code.utils.Image(path=img_path)
+noisy = image.noisy(code.utils.GaussianNoise(std=0.5))
+
+x = image.get()
+y = noisy.get()
+
+print code.noise.mse(x, y)
+print code.noise.psnr(x, y)
+print code.noise.ssim(x, y)
