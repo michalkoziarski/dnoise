6958edfa67fc55a93cc35a5067f8331512e83c28
Michał Koziarski <michalkoziarski@gmail.com>
2016-02-28 19:38:14 +0100

    modified training params

--- examples/stl_denoising.py
+++ examples/stl_denoising.py
@@ -5,4 +5,4 @@ from dnoise.noise import *
 
 ds = load_stl(shape=(96, 96), grayscale=True)
 cnn = Denoising(input_shape=[96, 96, 1], output_shape=[96, 96, 1])
-cnn.train(ds, epochs=100, noise=GaussianNoise(), visualize=5)
+cnn.train(ds, epochs=1000, noise=GaussianNoise(), visualize=10, display_step=250)
