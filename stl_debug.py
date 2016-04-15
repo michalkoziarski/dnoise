21f40816299047f6af8fbf4e2eff0ea28da9483a
Michał Koziarski <michalkoziarski@gmail.com>
2016-04-15 14:55:40 +0200

    debug - logging

--- stl_debug.py
+++ stl_debug.py
@@ -4,4 +4,4 @@ from dnoise.loaders import load_stl
 
 ds = load_stl(grayscale=False, batch_size=50, n=None)
 cnn = CNN(input_shape=[96, 96, 3], output_shape=[10])
-cnn.train(ds, debug=True, log=None, display_step=250, epochs=1000, learning_rate=0.01)
+cnn.train(ds, debug=True, log='stl_debug', display_step=250, epochs=1000, learning_rate=0.01)
