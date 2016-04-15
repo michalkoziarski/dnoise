880302ca07f37699c9649f1e875278f409da9044
Michał Koziarski <michalkoziarski@gmail.com>
2016-04-15 14:44:43 +0200

    debug - params for stl

--- stl_debug.py
+++ stl_debug.py
@@ -2,6 +2,6 @@ from dnoise.cnn import CNN
 from dnoise.loaders import load_stl
 
 
-ds = load_stl(grayscale=False, batch_size=10, n=10)
+ds = load_stl(grayscale=False, batch_size=50, n=None)
 cnn = CNN(input_shape=[96, 96, 3], output_shape=[10])
-cnn.train(ds, debug=True, log=None, display_step=1, epochs=1000, learning_rate=0.01)
+cnn.train(ds, debug=True, log=None, display_step=250, epochs=1000, learning_rate=0.01)
