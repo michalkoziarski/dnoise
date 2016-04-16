926ecf4b4c4b7a5915c5513a8e8c058701df204f
Michał Koziarski <michalkoziarski@gmail.com>
2016-04-16 15:38:46 +0200

    modified metaparams for restoring model

--- stl_restoring.py
+++ stl_restoring.py
@@ -4,6 +4,6 @@ from dnoise.noise import *
 
 
 ds = load_stl_unsupervised(shape=(64, 64), grayscale=True, batch_size=1)
-cnn = Restoring(input_shape=[64, 64, 1], output_shape=[58, 58, 1], weight_decay=0.)
+cnn = Restoring(input_shape=[64, 64, 1], output_shape=[58, 58, 1], weight_decay=0.2)
 cnn.train(ds, epochs=1000, noise=GaussianNoise(std=0.05), visualize=5, display_step=100000,
-          learning_rate=0.001, debug=True, score_samples=1000)
+          learning_rate=0.00001, debug=True, score_samples=1000)
