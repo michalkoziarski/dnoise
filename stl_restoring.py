686c0ef1c68f66041acdf828a6b3b8f4319ae91d
Michał Koziarski <michalkoziarski@gmail.com>
2016-04-16 16:20:42 +0200

    bigger filters in restoring model

--- stl_restoring.py
+++ stl_restoring.py
@@ -4,6 +4,6 @@ from dnoise.noise import *
 
 
 ds = load_stl_unsupervised(shape=(96, 96), grayscale=True, batch_size=1)
-cnn = Restoring(input_shape=[96, 96, 1], output_shape=[90, 90, 1], weight_decay=0.2)
+cnn = Restoring(input_shape=[96, 96, 1], output_shape=[84, 84, 1], weight_decay=0.2)
 cnn.train(ds, epochs=1000, noise=GaussianNoise(std=0.05), visualize=5, display_step=100000,
           learning_rate=0.00001, debug=True, score_samples=1000)
