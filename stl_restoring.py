83e7c27b43d60d38fd47d169f305654b14f87a97
Michał Koziarski <michalkoziarski@gmail.com>
2016-04-16 14:59:35 +0200

    enable debug in restoring model

--- stl_restoring.py
+++ stl_restoring.py
@@ -5,4 +5,5 @@ from dnoise.noise import *
 
 ds = load_stl_unsupervised(shape=(64, 64), grayscale=True, batch_size=1)
 cnn = Restoring(input_shape=[64, 64, 1], output_shape=[58, 58, 1], weight_decay=0.)
-cnn.train(ds, epochs=1000, noise=GaussianNoise(std=0.05), visualize=5, display_step=100000, learning_rate=0.001)
+cnn.train(ds, epochs=1000, noise=GaussianNoise(std=0.05), visualize=5, display_step=100000,
+          learning_rate=0.001, debug=True)
