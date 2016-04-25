675bf477619f03e0048420a51090ac9d9a4bf22f
Michał Koziarski <michalkoziarski@gmail.com>
2016-04-25 16:15:03 +0200

    updated stl classification script

--- stl.py
+++ stl.py
@@ -2,6 +2,6 @@ from dnoise.cnn import CNN
 from dnoise.loaders import load_stl
 
 
-ds = load_stl(grayscale=False, batch_size=50, n=None)
-cnn = CNN(input_shape=[96, 96, 3], output_shape=[10])
-cnn.train(ds, debug=True, log='stl_debug', display_step=250, epochs=1000, learning_rate=0.01)
+ds = load_stl(grayscale=True, batch_size=50)
+cnn = CNN(input_shape=[96, 96, 1], output_shape=[10])
+cnn.train(ds, debug=True, display_step=100, epochs=1000, learning_rate=0.01, results_dir='classification')
