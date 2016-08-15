c533bc9919e88115d3a25edf3c362700d9f33942
Michał Koziarski <michalkoziarski@gmail.com>
2016-08-15 15:38:50 +0200

    bash scripts

--- run_imagenet.sh
+++ run_imagenet.sh
@@ -1,5 +1,5 @@
-for lr in $(0.01 0.001 0.0001); do
-    for kernel in $(17 25 33); do
+for lr in 0.01 0.001 0.0001; do
+    for kernel in 17 25 33; do
         sbatch imagenet.sh lr kernel
     done
 done
