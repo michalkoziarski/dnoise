ee5fe22c6c37922267889d9232468b9576492768
Michał Koziarski <michalkoziarski@gmail.com>
2016-08-15 15:36:27 +0200

    bash scripts

---
+++ imagenet.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH --ntasks-per-node=12
+#SBATCH --time=72:00:00
+#SBATCH -p plgrid-gpu
+#SBATCH --gres=gpu
+
+module add plgrid/tools/python/2.7.9
+module add plgrid/apps/cuda/7.0
+
+python imagenet_motion_blur.py -lr=$1 -kernel=$2
