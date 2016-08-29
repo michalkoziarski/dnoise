8da368ec7c985d1eb2635832be6ffd44c99b82b0
Michał Koziarski <michalkoziarski@gmail.com>
2016-08-29 15:51:28 +0200

    fixed script name

--- experiments/motion_blur_removal.sh
+++ experiments/motion_blur_removal.sh
@@ -8,4 +8,4 @@
 module add plgrid/tools/python/2.7.9
 module add plgrid/apps/cuda/7.0
 
-python imagenet_motion_blur.py -learning_rate=$1 -kernel_size=$2 -weight_decay=$3
+python motion_blur_removal.py -learning_rate=$1 -kernel_size=$2 -weight_decay=$3
