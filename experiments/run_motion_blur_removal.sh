e6a3440d2a61addb5ae9f4054a000d6e9bbc32c2
Michał Koziarski <michalkoziarski@gmail.com>
2016-09-01 15:25:15 +0200

    removed run script

--- experiments/run_motion_blur_removal.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for learning_rate in 0.01; do
-    for kernel_size in 17 25 33; do
-        for weight_decay in 0.0002 0.0; do
-            sbatch script.sh motion_blur_removal.py --learning_rate ${learning_rate} --kernel_size ${kernel_size} --weight_decay ${weight_decay}
-        done
-    done
-done
