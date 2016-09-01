26fcf63380913d4a4ca9a2091d80a7beaf427969
Michał Koziarski <michalkoziarski@gmail.com>
2016-09-01 15:04:31 +0200

    rearranged bash scripts, still not satisfactory

--- experiments/run_motion_blur_removal.sh
+++ experiments/run_motion_blur_removal.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-for learning_rate in 0.01 0.001 0.0001; do
+for learning_rate in 0.01; do
     for kernel_size in 17 25 33; do
         for weight_decay in 0.0002 0.0; do
-            sbatch imagenet.sh ${learning_rate} ${kernel_size} ${weight_decay}
+            sbatch script.sh motion_blur_removal.py --learning_rate ${learning_rate} --kernel_size ${kernel_size} --weight_decay ${weight_decay}
         done
     done
 done
