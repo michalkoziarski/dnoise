2db151d91464df87270831f25bfa62f805012f96
Michał Koziarski <michalkoziarski@gmail.com>
2016-10-09 22:39:03 +0200

    imagenet classification script

--- experiments/imagenet_classification.py
+++ experiments/imagenet_classification.py
@@ -5,9 +5,62 @@ sys.path.append('..')
 import models
 import trainers
 import loaders
-import noise
 import tensorflow as tf
 import argparse
 
 
-train_set, test_set = loaders.load_imagenet_labeled(batch_size=50, patch=256)
+class Network(models.Network):
+    def setup(self):
+        self.conv(3, 3, self.input_shape[2], 64).\
+            pool().\
+            conv(3, 3, 64, 128).\
+            pool().\
+            conv(3, 3, 128, 256).\
+            conv(3, 3, 256, 256).\
+            pool().\
+            conv(3, 3, 256, 512).\
+            conv(3, 3, 512, 512).\
+            pool().\
+            conv(3, 3, 512, 512).\
+            conv(3, 3, 512, 512).\
+            pool().\
+            fully(4096).\
+            fully(4096).\
+            fully(1000).\
+            softmax()
+
+
+params = {
+    'learning_rate': 0.01,
+    'momentum': 0.9,
+    'weight_decay': 0.0002,
+    'batch_size': 50,
+    'epochs': 100,
+    'experiment': 'ImageNet - classification',
+    'summary_step': 10000
+}
+
+parser = argparse.ArgumentParser()
+
+for k in params.keys():
+    parser.add_argument('-%s' % k)
+
+args = vars(parser.parse_args())
+
+for k, v in params.iteritems():
+    if args.get(k) is not None and args.get(k) is not '':
+        params[k] = type(v)(args.get(k))
+
+params['trial'] = ', '.join(map(lambda (k, v): '%s = %s' % (k, v), params.iteritems()))
+
+network = Network([224, 224, 3], [1000])
+loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(network.logits, network.y_))
+correct_prediction = tf.equal(tf.argmax(network.y_, 1), tf.argmax(network.output(), 1))
+score = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+optimizer = tf.train.MomentumOptimizer(params['learning_rate'], params['momentum'])
+
+trainer = trainers.Trainer(params, network, loss, score, optimizer)
+
+train_set, test_set = loaders.load_imagenet_labeled(batch_size=params['batch_size'], patch=224)
+
+trainer.train(train_set, test_set=test_set)
