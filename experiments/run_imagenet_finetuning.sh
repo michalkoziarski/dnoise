ee719319a0c09c0a2e083c20306e6b1f225e24f3
michalkoziarski <michalkoziarski@gmail.com>
2017-01-17 22:04:13 +0100

    finetuning script

---
+++ experiments/run_imagenet_finetuning.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+noises=(
+    'GaussianNoise(0.05)'
+    'GaussianNoise(0.10)'
+    'GaussianNoise(0.20)'
+    'GaussianNoise(0.50)'
+    'QuantizationNoise(0.05)'
+    'QuantizationNoise(0.10)'
+    'QuantizationNoise(0.20)'
+    'QuantizationNoise(0.50)'
+    'SaltAndPepperNoise(0.05)'
+    'SaltAndPepperNoise(0.10)'
+    'SaltAndPepperNoise(0.20)'
+    'SaltAndPepperNoise(0.50)'
+    'RandomNoise(GaussianNoise)'
+    'RandomNoise(QuantizationNoise)'
+    'RandomNoise(SaltAndPepperNoise)'
+    'RandomNoise()'
+)
+
+for noise in "${noises[@]}"
+do
+    sbatch script.sh imagenet_finetuning.py -noise "$noise"
+done
