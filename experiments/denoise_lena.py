bcbaf0d85d05040a62bca1ab21a56d492409cdd4
michalkoziarski <michalkoziarski@gmail.com>
2017-01-24 21:06:37 +0100

    alternative way of finding trial path

--- experiments/denoise_lena.py
+++ experiments/denoise_lena.py
@@ -1,6 +1,5 @@
 import sys
 import os
-import hashlib
 import json
 import numpy as np
 import tensorflow as tf
@@ -83,15 +82,27 @@ if not os.path.exists(results_path):
 for noise in ['Gaussian', 'Quantization', 'SaltAndPepper']:
     for value in ['0.05', '0.10', '0.20', '0.50']:
         with tf.Session() as sess:
-            current_params = params.copy()
-            current_params['noise'] = '%sNoise(%s)' % (noise, value)
-            checkpoint_path = os.path.join(os.path.dirname(__file__), '..', 'results', params['experiment'],
-                                           hashlib.md5(str(current_params)).hexdigest())
+            experiment_path = os.path.join(os.path.dirname(__file__), '..', 'results', params['experiment'])
+            trial_paths = [os.path.join(experiment_path, o) for o in os.listdir(experiment_path)
+                           if os.path.isdir(os.path.join(experiment_path, o))]
+
+            trial_path = None
+
+            for path in trial_paths:
+                with open(os.path.join(path, 'params.json'), 'r') as fp:
+                    params = json.load(fp)
+
+                    if params['noise'] == ('%sNoise(%s)' % (noise, value)):
+                        trial_path = path
+
+                        break
+
+            assert trial_path is not None
 
             print('Trying to load model...')
-            print('Path: %s' % checkpoint_path)
+            print('Path: %s' % trial_path)
 
-            checkpoint = tf.train.get_checkpoint_state(checkpoint_path)
+            checkpoint = tf.train.get_checkpoint_state(trial_path)
             tf.train.Saver().restore(sess, checkpoint.model_checkpoint_path)
 
             image = Image(path=os.path.join(os.path.dirname(__file__), 'lena.jpg'))
