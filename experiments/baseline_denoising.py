5a6aff463d51fec21891357915f06465c44f1a7c
michalkoziarski <michalkoziarski@gmail.com>
2017-02-03 17:16:38 +0100

    storing images used for evaluation as well as the results

--- experiments/baseline_denoising.py
+++ experiments/baseline_denoising.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import json
 import numpy as np
 
 from pybm3d.bm3d import bm3d
@@ -16,7 +17,7 @@ def psnr(x, y, maximum=1.0):
     return 20 * np.log10(maximum) - 10 * np.log10(np.mean(np.power(x - y, 2)))
 
 
-def evaluate(noise, images):
+def evaluate(noise, images, path):
     methods = {
         'bm3d': [0.05, 0.1, 0.2, 0.3, 0.4, 0.5],
         'median': [3, 5, 7, 9, 11, 13],
@@ -36,9 +37,13 @@ def evaluate(noise, images):
         for value in methods[method]:
             result[method][value] = []
 
-    for image in images:
+    for i in range(len(images)):
+        image = images[i]
+        image.display(os.path.join(path, 'Clean_%d.jpg' % i))
         clean = image.get()
-        noisy = image.noisy(noise).get().astype(np.float32)
+        noisy = image.noisy(eval(noise))
+        noisy.display(os.path.join(path, '%s_%d.jpg' % (noise, i)))
+        noisy = noisy.get().astype(np.float32)
 
         result['input'].append(psnr(clean, noisy))
 
@@ -66,18 +71,24 @@ def evaluate(noise, images):
     return result
 
 
+results_path = os.path.join(os.path.dirname(__file__), '..', 'results', 'baseline')
+
+if not os.path.exists(results_path):
+    os.mkdir(results_path)
+
 results = {}
 images = load_imagenet_unlabeled_validation(batch_size=1).images[:100]
 
 for noise_type in ['Gaussian', 'Quantization', 'SaltAndPepper']:
     for value in [0.05, 0.1, 0.2, 0.5]:
         noise = '%sNoise(%s)' % (noise_type, value)
-        results[noise] = evaluate(eval(noise), images)
+        results[noise] = evaluate(noise, images, results_path)
 
     noise = 'RandomNoise(%sNoise)' % noise_type
-    results[noise] = evaluate(eval(noise), images)
+    results[noise] = evaluate(noise, images, results_path)
 
 noise = 'RandomNoise()'
-results[noise] = evaluate(eval(noise), images)
+results[noise] = evaluate(noise, images, results_path)
 
-print(results)
+with open(os.path.join(results_path, 'PSNR.json'), 'w') as fp:
+    json.dump(results, fp)
