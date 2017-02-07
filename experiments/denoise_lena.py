97248360597ce1a014e692bd8719ce7ab567a79c
michalkoziarski <michalkoziarski@gmail.com>
2017-02-07 11:58:20 +0100

    removed redeclaration of RGBNetwork

--- experiments/denoise_lena.py
+++ experiments/denoise_lena.py
@@ -10,7 +10,7 @@ import models
 
 from noise import GaussianNoise, QuantizationNoise, SaltAndPepperNoise
 from containers import Image
-from imagenet_denoising import params
+from imagenet_denoising import params, RGBNetwork
 
 
 results = {
@@ -20,59 +20,12 @@ results = {
 }
 
 
-class SingleChannelNetwork(models.Network):
-    def __init__(self, input_shape, output_shape):
-        self.input_shape = input_shape
-        self.output_shape = output_shape
-        self.x = None
-        self.y_ = None
-        self.weights = []
-        self.biases = []
-
-    def setup(self):
-        self.conv(5, 5, self.input_shape[2], 48, activation=tf.nn.tanh). \
-            conv(5, 5, 48, 48, activation=tf.nn.tanh). \
-            conv(5, 5, 48, 48, activation=tf.nn.tanh). \
-            conv(5, 5, 48, 48, activation=tf.nn.tanh). \
-            conv(5, 5, 48, 48, activation=tf.nn.tanh). \
-            conv(5, 5, 48, 48, activation=tf.nn.tanh). \
-            conv(5, 5, 48, self.output_shape[2], activation=None)
-
-
-class RGBNetwork:
-    def __init__(self, input_shape, output_shape, x=None):
-        self.input_shape = input_shape
-        self.output_shape = output_shape
-        self.networks = [SingleChannelNetwork(input_shape[:2] + [1], output_shape[:2] + [1]) for _ in range(3)]
-
-        if x is None:
-            self.x = tf.placeholder(tf.float32, shape=[None] + input_shape)
-        else:
-            self.x = x
-
-        self.y_ = tf.placeholder(tf.float32, shape=[None] + output_shape)
-        self.keep_prob = tf.placeholder(tf.float32)
-        self.weights = []
-        self.biases = []
-
-        for i in range(3):
-            self.networks[i].x = tf.slice(self.x, [0, 0, 0, i], [-1, -1, -1, 1])
-            self.networks[i].y_ = tf.slice(self.y_, [0, 0, 0, i], [-1, -1, -1, 1])
-            self.networks[i].layers = [self.networks[i].x]
-            self.networks[i].setup()
-            self.weights += self.networks[i].weights
-            self.biases += self.networks[i].biases
-
-    def output(self):
-        return tf.concat(3, [network.output() for network in self.networks])
-
-
 def psnr(x, y):
     return 20 * np.log10(params['scale'][1]) - 10 * tf.log(
         tf.maximum(tf.reduce_mean(tf.pow(x - y, 2)), 1e-20)) / np.log(10)
 
 
-network = RGBNetwork([224, 224, 3], [224, 224, 3])
+network = RGBNetwork()
 
 results_path = os.path.join(os.path.dirname(__file__), '..', 'results', 'Lena')
 
